03e393aa5922e1aa9b8f57b66c78ff6fc6c32891
Konstantin <soonts@live.com>
2019-06-30 11:52:26 +0200

    Minor, disabled debugging stuff

--- src/FontStash2/debugSaveGlyphs.h
+++ src/FontStash2/debugSaveGlyphs.h
@@ -4,9 +4,9 @@ typedef struct FT_GlyphSlotRec_*  FT_GlyphSlot;
 
 namespace FontStash2
 {
-#ifdef _MSC_VER
+#if 0
 	bool debugSaveGlyph( const FT_GlyphSlot data, uint32_t index, float size, const char* suffix );
 #else
-	inline bool debugSaveGlyph( const FT_GlyphSlot data, uint32_t index, float size, const char* suffix ) { }
+	inline bool debugSaveGlyph( const FT_GlyphSlot data, uint32_t index, float size, const char* suffix ) { return true; }
 #endif
 }
